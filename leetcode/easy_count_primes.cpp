ba4518c83a9dfb3fff900ae37bcb552634fa6d27
wonghoifung <wonghoifung85@gmail.com>
2015-12-08 04:31:40 -0500

    count primes

---
+++ leetcode/easy_count_primes.cpp
@@ -0,0 +1,74 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <sstream>
+#include <algorithm>
+#include <bitset>
+#include <numeric>
+#include <math.h>
+#include <assert.h>
+#include <time.h>
+#include <stdint.h>
+
+using namespace std;
+
+class mytimer
+{
+private:
+	time_t start_;
+public:
+	mytimer()
+	{
+		start_ = time(NULL);
+	}
+	~mytimer()
+	{
+		time_t end = time(NULL);
+		cout << end - start_ << "s" << endl;
+	}
+};
+
+class Solution {
+public:
+	/* Sieve of Eratosthenes */
+	int countPrimes(int n) {
+		bool* isPrime = new bool[n];
+		isPrime[0] = false;
+		isPrime[1] = false;
+		for (int i = 2; i < n; i++) {
+			isPrime[i] = true;
+		}
+		for (int i = 2; i * i < n; i++) {
+			if (!isPrime[i]) continue;
+			for (int j = i * i; j < n; j += i) {
+				isPrime[j] = false;
+			}
+		}
+		int count = 0;
+		for (int i = 2; i < n; i++) {
+			if (isPrime[i]) count++;
+		}
+		delete[] isPrime;
+		return count;
+	}
+};
+
+int main()
+{
+	Solution s;
+	{
+		mytimer t;
+		cout << s.countPrimes(1500000) << endl;
+	}
+	std::cin.get();
+
+	return 0;
+}
+
