fb382b48f0757dc6d262d95d782d6f16c9792118
wonghoifung <wonghoifung85@gmail.com>
2016-02-02 14:23:20 +0800

    longest palindromic substring

---
+++ leetcode/medium_longest_palindromic_substring.cpp
@@ -0,0 +1,64 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <array>
+#include <memory>
+#include <sstream>
+#include <functional>
+#include <algorithm>
+#include <bitset>
+#include <tuple>
+#include <numeric>
+#include <initializer_list>
+#include <math.h>
+#include <assert.h>
+#include <time.h>
+#include <stdint.h>
+
+using namespace std;
+
+class Solution {
+public:
+	string helper(string s, int l, int r) {
+		while (l >= 0 && r < s.size() && s[l] == s[r])
+		{
+			l -= 1;
+			r += 1;
+		}
+		return s.substr(l + 1, r - l - 1);
+	}
+	string longestPalindrome(string s) {
+		string res;
+		for (int i = 0; i < s.size(); ++i)
+		{
+			string tmp = helper(s, i, i);
+			if (tmp.size() > res.size()) res = tmp;
+
+			tmp = helper(s, i, i + 1);
+			if (tmp.size() > res.size()) res = tmp;
+		}
+		return res;
+	}
+};
+
+int main()
+{
+	Solution s;
+	{
+		cout << s.longestPalindrome("cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc") << endl;
+	}
+
+	std::cin.get();
+	return 0;
+}
+
+
+
+
