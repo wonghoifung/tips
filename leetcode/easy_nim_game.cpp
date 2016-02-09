7719020814170846a16dde48769f79c64ffb2a13
wonghoifung <wonghoifung85@gmail.com>
2016-02-09 09:09:18 +0800

    nim game

---
+++ leetcode/easy_nim_game.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <deque>
+#include <algorithm> 
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <set>
+#include <map>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canWinNim(int n) {
+        return n % 4 > 0;
+    }
+};
+
+int main(int argc, char** argv)
+{
+
+
+    return 0;
+}
+
