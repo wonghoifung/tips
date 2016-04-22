60b1a11e311d094fe75a079df82887f09d8f09db
wonghoifung <wonghoifung85@gmail.com>
2016-04-22 12:23:26 +0800

    reverse string

---
+++ leetcode/easy_reverse_string.cpp
@@ -0,0 +1,47 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <array>
+#include <memory>
+#include <sstream>
+#include <functional>
+#include <algorithm>
+#include <bitset>
+#include <tuple>
+#include <numeric>
+#include <initializer_list>
+#include <math.h>
+#include <assert.h>
+#include <time.h>
+#include <stdint.h>
+#include <stdarg.h>
+
+using namespace std;
+
+class Solution {
+public:
+	string reverseString(string s) {
+		reverse(s.begin(), s.end());
+		return s;
+	}
+};
+
+int main()
+{
+	Solution s;
+	cout << s.reverseString("hello") << endl;
+
+	std::cin.get();
+	return 0;
+}
+
+
+
+
