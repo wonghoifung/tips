5d3f1a4a676cea9d31a29463c88ceabfc74d2994
wonghoifung <wonghoifung85@gmail.com>
2016-05-11 18:05:35 +0800

    compile

--- lua/redis/build_unittest_redisClient.sh
+++ lua/redis/build_unittest_redisClient.sh
@@ -1,5 +1,2 @@
-git clone https://github.com/redis/hiredis.git
-cd hiredis
-make
-cd ..
+
 g++ -I./hiredis/ -L./hiredis/ -lhiredis redisClient.cpp unittest_redisClient.cpp -o unittest_redisClient
---
+++ lua/redis/get_hiredis.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/redis/hiredis.git
+cd hiredis
+make
