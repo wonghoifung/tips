456b7c9b4ca79f844f21e13785eac044bea80c43
wonghoifung <wonghoifung85@gmail.com>
2016-05-29 07:32:08 +0800

    intersection of two arrays

---
+++ leetcode/easy_intersection_of_two_arrays.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> res;
+        unordered_map<int,int> num2cnt;
+        for (auto i=0; i<nums1.size(); ++i) {
+        	num2cnt[nums1[i]] = 1;
+        } 
+        for (auto i=0; i<nums2.size(); ++i) {
+        	if (num2cnt[nums2[i]] == 1) {
+        		num2cnt[nums2[i]] = 0;
+        		res.push_back(nums2[i]);
+        	}
+        }
+        return res;
+    }
+};
+
+int main() {
+	Solution s;
+	{
+		vector<int> nums1{1,2,2,1};
+		vector<int> nums2{2,2};
+		vector<int> res = s.intersection(nums1,nums2);
+		for (auto i=0; i<res.size(); ++i) {
+			cout << res[i] << endl;
+		}
+	}
+}
