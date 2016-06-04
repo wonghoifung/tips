96f077b91ab7d3ef1dc1017039fc2c5fb1855fb8
wonghoifung <wonghoifung85@gmail.com>
2016-06-04 20:45:38 +0800

    .

--- jvmgo/main.go
+++ jvmgo/main.go
@@ -1,6 +1,8 @@
 package main 
 
 import "fmt"
+import "strings"
+import "jvmgo/classpath"
 
 func main() {
 	cmd := parseCmd()
@@ -14,5 +16,13 @@ func main() {
 }
 
 func startJVM(cmd *Cmd) {
-	fmt.Printf("classpath: %s class: %s args: %v\n", cmd.cpOption, cmd.class, cmd.args)
+	cp := classpath.Parse(cmd.XjreOption, cmd.cpOption)
+	fmt.Printf("classpath:%v class:%v args:%v\n", cp, cmd.class, cmd.args)
+	className := strings.Replace(cmd.class, ".", "/", -1)
+	classData, _, err := cp.ReadClass(className)
+	if err != nil {
+		fmt.Printf("could not find or load main class %s\n", cmd.class)
+		return
+	}
+	fmt.Printf("class data:%v\n", classData)
 }
