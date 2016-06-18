91b6838b371413b3028ebc332301669994c23c92
wonghoifung <wonghoifung85@gmail.com>
2016-06-18 11:12:03 +0800

    .

--- goworkspace/src/jvmgo/main.go
+++ goworkspace/src/jvmgo/main.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 import "strings"
+import "jvmgo/classfile"
 import "jvmgo/classpath"
 
 func main() {
@@ -16,13 +17,49 @@ func main() {
 }
 
 func startJVM(cmd *Cmd) {
+	// cp := classpath.Parse(cmd.XjreOption, cmd.cpOption)
+	// fmt.Printf("classpath:%v class:%v args:%v\n", cp, cmd.class, cmd.args)
+	// className := strings.Replace(cmd.class, ".", "/", -1)
+	// classData, _, err := cp.ReadClass(className)
+	// if err != nil {
+	// 	fmt.Printf("could not find or load main class %s\n", cmd.class)
+	// 	return
+	// }
+	// fmt.Printf("class data:%v\n", classData)
+
 	cp := classpath.Parse(cmd.XjreOption, cmd.cpOption)
-	fmt.Printf("classpath:%v class:%v args:%v\n", cp, cmd.class, cmd.args)
 	className := strings.Replace(cmd.class, ".", "/", -1)
+	cf := loadClass(className, cp)
+	fmt.Println(cmd.class)
+	printClassInfo(cf)
+}
+
+func loadClass(className string, cp *classpath.Classpath) *classfile.ClassFile {
 	classData, _, err := cp.ReadClass(className)
 	if err != nil {
-		fmt.Printf("could not find or load main class %s\n", cmd.class)
-		return
+		panic(err)
 	}
-	fmt.Printf("class data:%v\n", classData)
+	cf, err := classfile.Parse(classData)
+	if err != nil {
+		panic(err)
+	}
+	return cf
 }
+
+func printClassInfo(cf *classfile.ClassFile) {
+	fmt.Printf("version: %v.%v\n", cf.MajorVersion(), cf.MinorVersion())
+	fmt.Printf("constants count: %v\n", len(cf.ConstantPool()))
+	fmt.Printf("access flags: 0x%x\n", cf.AccessFlags())
+	fmt.Printf("this class: %v\n", cf.ClassName())
+	fmt.Printf("super class: %v\n", cf.SuperClassName())
+	fmt.Printf("interfaces: %v\n", cf.InterfaceNames())
+	fmt.Printf("fields count: %v\n", len(cf.Fields()))
+	for _, f := range cf.Fields() {
+		fmt.Printf("  %s\n", f.Name())
+	}
+	fmt.Printf("methods count: %v\n", len(cf.Methods()))
+	for _, m := range cf.Methods() {
+		fmt.Printf("  %s\n", m.Name())
+	}
+}
+
