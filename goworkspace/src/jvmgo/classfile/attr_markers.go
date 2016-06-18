1e9a357f5d6378875a63cea334f773bc9c5a4e63
wonghoifung <wonghoifung85@gmail.com>
2016-06-18 10:35:46 +0800

    .

---
+++ goworkspace/src/jvmgo/classfile/attr_markers.go
@@ -0,0 +1,17 @@
+package classfile
+
+type DeprecatedAttribute struct {
+	MarkerAttribute
+}
+
+type SyntheticAttribute struct {
+	MarkerAttribute
+}
+
+type MarkerAttribute struct {
+
+}
+
+func (self *MarkerAttribute) readInfo(reader *ClassReader) {
+
+}
