b8c0e66eb7a65f7bc79809f51caaa74abe076cca
wonghoifung <wonghoifung85@gmail.com>
2016-06-24 07:55:32 +0800

    .

---
+++ goworkspace/src/jvmgo/instructions/constants/ipush.go
@@ -0,0 +1,30 @@
+package constants
+
+import "jvmgo/instructions/base"
+import "jvmgo/rtda"
+
+type BIPUSH struct {
+	val int8
+}
+
+type SIPUSH struct {
+	val int16
+}
+
+func (self *BIPUSH) FetchOperands(reader *base.BytecodeReader) {
+	self.val = reader.ReadInt8()
+}
+
+func (self *BIPUSH) Execute(frame *rtda.Frame) {
+	i := int32(self.val)
+	frame.OperandStack().PushInt(i)
+}
+
+func (self *SIPUSH) FetchOperands(reader *base.BytecodeReader) {
+	self.val = reader.ReadInt16()
+}
+
+func (self *SIPUSH) Execute(frame *rtda.Frame) {
+	i := int32(self.val)
+	frame.OperandStack().PushInt(i)
+}
