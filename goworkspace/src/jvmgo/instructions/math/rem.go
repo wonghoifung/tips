19ffbb5b77770ab363b6f98279e052ed9571c81c
wonghoifung <wonghoifung85@gmail.com>
2016-06-28 07:58:26 +0800

    .

---
+++ goworkspace/src/jvmgo/instructions/math/rem.go
@@ -0,0 +1,59 @@
+package math
+
+import "math"
+import "jvmgo/instructions/base"
+import "jvmgo/rtda"
+
+type DREM struct {
+	base.NoOperandsInstruction
+}
+
+type FREM struct {
+	base.NoOperandsInstruction
+}
+
+type IREM struct {
+	base.NoOperandsInstruction
+}
+
+type LREM struct {
+	base.NoOperandsInstruction
+}
+
+func (self *IREM) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopInt()
+	v1 := stack.PopInt()
+	if v2 == 0 {
+		panic("java.lang.ArithmeticException: / by zero")
+	}
+	result := v1 % v2
+	stack.PushInt(result)
+}
+
+func (self *LREM) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopLong()
+	v1 := stack.PopLong()
+	if v2 == 0 {
+		panic("java.lang.ArithmeticException: / by zero")
+	}
+	result := v1 % v2
+	stack.PushLong(result)
+}
+
+func (self *DREM) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopDouble()
+	v1 := stack.PopDouble()
+	result := math.Mod(v1, v2)
+	stack.PushDouble(result)
+}
+
+func (self *FREM) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopFloat()
+	v1 := stack.PopFloat()
+	result := math.Mod(v1, v2)
+	stack.PushFloat(result)
+}
