11efb28a859f8b038eead47387a85b534d497380
wonghoifung <wonghoifung85@gmail.com>
2016-07-28 11:13:10 +0800

    weak table

--- luatest/test.lua
+++ luatest/test.lua
@@ -9,3 +9,46 @@ print(f())
 local tlh = require "testlib.luahello"
 tlh.hello()
 print(tlh.three())
+
+-- weak table 
+wt = {}
+wt[1] = function() print("first call") end
+wt[2] = function() print("second call") end
+wt[3] = {1,2,3}
+setmetatable(wt,{__mode="kv"})
+print(#wt)
+ele=wt[1]
+collectgarbage()
+print(#wt)
+ele=nil
+collectgarbage()
+print(#wt)
+
+-- create table with default value, only one metatable, but weak table is large
+local defaults={}
+setmetatable(defaults, {__mode="k"})
+local mt = {__index=function(t) return defaults[t] end}
+function setDefault( t, d )
+	defaults[t] = d
+	setmetatable(t, mt)
+end
+local ttt={}
+setDefault(ttt, 123)
+print(ttt[1])
+print(ttt[2])
+
+-- create table with default value 2, many metatable, but weak table is small
+metas = {}
+setmetatable(metas, {__mode="v"})
+setDefault2 = function ( t, d )
+	local mt = metas[d]
+	if mt == nil then
+		mt = {__index = function() return d end}
+		metas[d] = mt
+	end
+	setmetatable(t, mt)
+end
+local ttt2={}
+setDefault2(ttt2, 456)
+print(ttt2[1])
+print(ttt2[33])
