9ff4aa4056ae71801fc07738fee9c755c417edd2
wonghoifung <wonghoifung85@gmail.com>
2016-08-04 10:53:32 +0800

    for function,arg

--- luatest/gate.lua
+++ luatest/gate.lua
@@ -7,3 +7,15 @@ gateserver.hello()
 gateserver.hello()
 
 require "gate2"
+
+local q={}
+table.insert(q,1)
+table.insert(q,2)
+table.insert(q,3)
+function pop(queue)
+	e = table.remove(queue)
+	return e
+end
+for e in pop,q do
+	print(e)
+end
