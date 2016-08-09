dbaa5983b6d5bfc0cca89d3bb4c8e45d4b09d78f
wonghoifung <wonghoifung85@gmail.com>
2016-08-09 11:27:30 +0800

    pointer

--- test_pointer.cpp
+++ test_pointer.cpp
@@ -11,12 +11,19 @@ struct Y {int a; char b; int c; char d;};
 
 struct Z {int i; int a[0];};
 
+class C {
+public:
+  int a;
+  char b;
+  static int c;
+};
+
 int main() {
   int a[10];
   void* p = (void*)1024;
   void* q = 0;
   printf("sizeof int: %d, sizeof void*: %d\n", sizeof(int), sizeof(void*));
-  printf("p-q=%d\n",p-q);
+  //printf("p-q=%d\n",p-q);
   printf("address: %p, %p, %p\n", &a[0], a, &a);
   printf("address: %p, %p\n", a+1, &a+1);
 
@@ -36,10 +43,11 @@ int main() {
   {
     void* p = malloc(100);
     printf("%d\n", sizeof(p)==8);
-    printf("%d\n", sizeof('A')==4);
+    printf("%d\n", sizeof('A')==1); // different from c
     printf("%d\n", sizeof(struct X)==12);
     printf("%d\n", sizeof(struct Y)==16);
     printf("%d\n", sizeof(struct Z)==4);
+    printf("%d\n", sizeof(C)==8);
   }
 }
 
