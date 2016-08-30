d6f33537c1549b16f08c28d1b4785091f1ba6364
wonghoifung <wonghoifung85@gmail.com>
2016-08-30 08:57:56 +0800

    buf chan

--- goworkspace/src/test/main.go
+++ goworkspace/src/test/main.go
@@ -16,5 +16,36 @@ func main() {
     cstr := C.CString(str)
     C.p(cstr)
     C.free(unsafe.Pointer(cstr))
+
+    //////////////
+
+    done := make(chan struct{})
+
+    data := []chan int{
+    	make(chan int, 3),
+    }
+
+    go func() {
+    	defer close(done)
+    	for i:=0; i<10; i++ {
+    		select {
+    		case data[len(data)-1] <- i:
+    		default:
+    			data = append(data, make(chan int, 3))
+    		}
+    	}
+    }()
+
+    <- done
+
+    fmt.Printf("len(data): %d\n", len(data)) // 4
+    for i:=0; i<len(data); i++ {
+    	c := data[i]
+    	close(c)
+    	fmt.Printf("---> i:%d\n", i)
+    	for x := range c {
+    		fmt.Println(x)
+    	}
+    }
 }
 
