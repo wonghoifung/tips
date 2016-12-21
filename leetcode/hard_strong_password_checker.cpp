47f01b51d61b678a8c8a510cadbb97335030f249
wonghoifung <wonghoifung85@gmail.com>
2016-12-20 23:24:50 -0500

    TODO strong password checker

---
+++ leetcode/hard_strong_password_checker.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+#include <iostream>
+#include <unordered_set>
+#include <unordered_map>
+#include <map>
+#include <sstream>
+#include <algorithm>
+#include <queue>
+#include <set>
+#include <functional>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+#if 0
+It has at least 6 characters and at most 20 characters.
+It must contain at least one lowercase letter, at least one uppercase letter, and at least one digit.
+It must NOT contain three repeating characters in a row("...aaa..." is weak, but "...aa...a..." is strong, assuming other conditions are met).
+#endif
+
+class Solution {
+public:
+	int strongPasswordChecker(string s) // TODO
+	{
+		
+	}
+};
+
+int main() {
+	{
+
+	}
+
+	cin.get();
+	return EXIT_SUCCESS;
+}
+
