c54084e94322ee4e61d371b704acc62757176c4e
wonghoifung <wonghoifung85@gmail.com>
2016-12-29 23:34:40 -0500

    todo

--- leetcode/hard_kth_smallest_in_lexicographical_order.cpp
+++ leetcode/hard_kth_smallest_in_lexicographical_order.cpp
@@ -19,7 +19,7 @@ class Solution {
 	int calSteps(int n, long n1, long n2) {
 		int steps = 0;
 		while (n1 <= n) {
-			steps += getMin(n + 1, n2) - n1;
+			steps += getMin(n + 1, n2) - n1; // TODO why?
 			n1 *= 10;
 			n2 *= 10;
 		}
@@ -30,7 +30,7 @@ public:
 		int curr = 1;
 		k = k - 1;
 		while (k > 0) {
-			int steps = calSteps(n, curr, curr + 1);
+			int steps = calSteps(n, curr, curr + 1); // TODO why?
 			if (steps <= k) {
 				curr += 1; // move to the next right neighbor
 				k -= steps;
