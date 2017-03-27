50cb94f818927e4d4f721de2649be57fe6c9466e
WongHoiFung (HuangHaiFeng/黄海峰) <wonghoifung85@gmail.com>
2017-03-27 17:34:26 +0800

    Create medium_longest_palindromic_subsequence.cpp

---
+++ leetcode/medium_longest_palindromic_subsequence.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+	int longestPalindromeSubseq(string s) {
+        int n = s.size();
+        vector<vector<int> > mem(n,vector<int>(n));
+        return longestPalindromeSubseq(0,n-1, s,mem); 
+    }
+    int longestPalindromeSubseq(int l, int r, string &s, vector<vector<int> >& mem) {
+        if(l==r) return 1;
+        if(l>r) return 0;
+        if(mem[l][r]) return mem[l][r];
+        return mem[l][r] = s[l]==s[r] ? 2 + longestPalindromeSubseq(l+1,r-1, s,mem) : 
+            max(longestPalindromeSubseq(l+1,r, s,mem),longestPalindromeSubseq(l,r-1, s,mem)); 
+        
+    }
+};
+int main() {
+	cout << Solution().longestPalindromeSubseq("bbbab") << endl; // 4
+	cout << Solution().longestPalindromeSubseq("cbbd") << endl; // 2
+}
