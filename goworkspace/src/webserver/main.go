82eb55b43446fcbeef3f95bbfabc0665367d0cfa
wonghoifung <wonghoifung85@gmail.com>
2017-05-20 08:28:25 +0800

    .

---
+++ goworkspace/src/webserver/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+  "strings"
+  "log"
+)
+
+func sayHi(w http.ResponseWriter, r *http.Request) {
+  r.ParseForm()
+  fmt.Println(r.Form)
+  fmt.Println("path",r.URL.Path)
+  fmt.Println("scheme",r.URL.Scheme)
+  fmt.Println(r.Form["url_long"])
+  for k,v := range r.Form {
+    fmt.Println("key:",k)
+    fmt.Println("val:",strings.Join(v,""))
+  }
+  fmt.Fprintf(w,"hi man")
+}
+
+func main() {
+  http.HandleFunc("/",sayHi)
+  err := http.ListenAndServe(":9090",nil)
+  if err != nil {
+    log.Fatal("ListerAndServe: ",err)
+  }
+}
+
