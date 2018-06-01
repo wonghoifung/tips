96ce8ce7d43cdb0798e33881d6986c31ab6e55d2
WongHoiFung (HuangHaiFeng/黄海峰) <wonghoifung85@gmail.com>
2018-06-01 17:25:36 +0800

    Update medium_task_scheduler.cpp

--- medium_task_scheduler.cpp
+++ medium_task_scheduler.cpp
@@ -8,16 +8,18 @@ using namespace std;
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        unordered_map<char,int> mp;
+        map<char,int> mp;
         int count = 0;
-        for(auto e : tasks)
+        for (int i=0; i<tasks.size(); ++i)
         {
-            mp[e]++;
-            count = max(count, mp[e]);
+            mp[tasks[i]]++;
+            count = max(count, mp[tasks[i]]);
         }
         
         int ans = (count-1)*(n+1);
-        for(auto e : mp) if(e.second == count) ans++;
+        for (map<char,int>::iterator it=mp.begin(); it!=mp.end(); ++it) {
+            if (it->second == count) ans+=1;
+        }
         return max((int)tasks.size(), ans);
     }
 };
